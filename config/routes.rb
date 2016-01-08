9d9af323d2f3d56b4867f526e62e69ae67ccde87
zhiguangxu <xuzhiguang@gmail.com>
2016-01-08 16:54:50 +0000

    Done with login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   resources :text_posts
   resources :posts
   resources :users
+  resources :sessions
   
   get 'signup', to: 'users#new', as: 'signup'
-  
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
